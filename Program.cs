dcae8428fd023cd824b94ddca9d5cfd30c845563
psssvj-arch <psssvj@gmail.com>
2026-09-14 22:01:25 -0300

    menu QUASE PRONTO

--- Program.cs
+++ Program.cs
@@ -15,6 +15,7 @@
     string opcao = Console.ReadLine();
 
     switch (opcao)
+
     {
         case "1":
             while (true)
@@ -32,24 +33,49 @@
 
                 Jogo_MySql jogo = new Jogo_MySql(nomeJogo, franquiaJogo, generoJogo, dataLancamentoJogo, notaJogo);
             }
+
         case "2":
-            // Lógica para listar jogos
-            break;
+            while (true)
+            {
+                Console.Write("Digite o nome do jogo que deseja buscar: ");
+                string nomeJogo = Console.ReadLine();
+                Console.Write("Digite a franquia do jogo que deseja buscar: ");
+                string franquiaJogo = Console.ReadLine();
+                Console.Write("Digite o gênero do jogo que deseja buscar: ");
+                string generoJogo = Console.ReadLine();
+
+            //precisa criar um método para buscar os jogos no banco de dados
+            }
+           
+
         case "3":
-            // Lógica para atualizar jogo
-            break;
+            while (true)
+            {
+                Console.Write("Digite o nome do jogo que deseja atualizar: ");
+                string nomeJogo = Console.ReadLine();
+                Console.Write("Digite a franquia do jogo que deseja atualizar: ");
+                string franquiaJogo = Console.ReadLine();
+                Console.Write("Digite o gênero do jogo que deseja atualizar: ");
+                string generoJogo = Console.ReadLine();
+                Console.Write("Digite a nova data de lançamento do jogo (DD/MM/AAAA): ");
+                string dataLancamentoJogo = Console.ReadLine();
+                Console.Write("Digite a nova nota do jogo: ");
+                double notaJogo = double.Parse(Console.ReadLine());
+
+                Jogo_MySql jogo = new Jogo_MySql(nomeJogo, franquiaJogo, generoJogo, dataLancamentoJogo, notaJogo);
+            }
+
         case "4":
             // Lógica para deletar jogo
             break;
+
         case "5":
             // Lógica para sair
             break;
+
         default:
             Console.WriteLine("Opção inválida!");
             continue;
     }
-
-
-
 
 } 
